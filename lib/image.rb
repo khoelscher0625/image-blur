461675efffa3404d6d00eb171647216c1c5ab654
Kathryn Hoelscher <khoelscher0625@gmail.com>
2020-01-23 02:32:12 +0000

    attempting to fix test for image blur 1

--- lib/image.rb
+++ lib/image.rb
@@ -6,7 +6,59 @@ class Image
   end
 
   def blur
-    #bad code
     return Image.new(@data)
   end
+
+   def transform (prev_row, curr_row, next_row)
+    new_row = []
+
+    curr_row.each_with_index do |val, index|
+      if val == 1
+        new_row << 1
+      elsif prev_row[index] == 1
+        new_row << 1
+      elsif next_row[index] == 1
+        new_row << 1
+      elsif curr_row[index - 1] == 1 && index - 1 != -1
+        new_row << 1
+      elsif curr_row[index + 1] == 1
+        new_row << 1
+      else 
+        new_row << 0
+      end
+    end
+      return new_row
+      
+  end
+
+  def output 
+    final_array = []
+  
+    @rows.each_with_index do |row, index|
+      prev_row = @rows[index - 1]
+        #if index - 1 = -1 then []
+        if index - 1 == -1
+          prev_row = []
+        end
+      next_row = @rows[index + 1]
+        if index + 1 == 4
+          next_row = []
+        end
+      new_row = self.transform(prev_row, row, next_row)             
+                    #changed rows
+    final_array << new_row
+    end
+    return final_array
+
+  end
 end
+
+
+image1 = Image.new([
+[0, 0, 0, 0],
+[0, 1, 0, 0],
+[0, 0, 0, 1],
+[0, 0, 0, 0]
+])
+image1.output
+
